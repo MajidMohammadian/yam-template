a494a7eddbc5cbe0f6ec6ec0489b4aae5acfcfa2
majidmohammadian <majeedmohammadian@gmail.com>
2025-03-30 15:02:16 +0330

    changed tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}",
-    "./**/*.html"
+    "./build/**/*.{html,js}",
   ],
   theme: {
     extend: {
       colors: {
-        primary: '#f58220',
-        lightPrimary: '#fb923c33',
-        brown: '#431407',
-        hoverPrimary: 'f97316',
-        yellow :'f58220'
+        primary: "#f58220",
+        lightPrimary: "#fb923c33",
+        hoverPrimary: "#f97316",
+        brown: "#431407",
+        yellow :"#f58220"
       },
     },
   },
